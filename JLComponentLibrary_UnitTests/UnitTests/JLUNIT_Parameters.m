adc925ff75e2e4c3678cd22b00a9c6871d50770f
Jack Lawrence <jlawrence@mail.rit.edu>
2012-11-08 10:45:31 -0500

    Improved NSURL parameter unit testing.

--- JLComponentLibrary_UnitTests/UnitTests/JLUNIT_Parameters.m
+++ JLComponentLibrary_UnitTests/UnitTests/JLUNIT_Parameters.m
@@ -10,38 +10,38 @@
 
 #import "NSURL+JL_Parameters.h"
 
-#define AssertExpectedHost(_URLObject, _host) { \
+#define AssertExpectedHost(_URLObject, _host) \
     STAssertNotNil(_URLObject, @"-[NSURL JL_URLWithProtocol:domain:endpoint:parameters:] returned a nil object."); \
-    STAssertEqualObjects([_URLObject host], _host, @"Expected host @\"%@\" but got @\"%@\"", _host, [_URLObject host]); \
-}
+    STAssertEqualObjects([_URLObject host], _host, @"Expected host @\"%@\" but got @\"%@\"", _host, [_URLObject host]);
 
-#define AssertExpectedAbsoluteURLString(_URLObject, _URLString) { \
+#define AssertExpectedAbsoluteURLString(_URLObject, _URLString) \
     STAssertNotNil(_URLObject, @"-[NSURL JL_URLWithProtocol:domain:endpoint:parameters:] returned a nil object."); \
-    STAssertEqualObjects([_URLObject absoluteString], _URLString, @"Expected absolute URL string @\"%@\" but got @\"%@\"", _URLString, [_URLObject absoluteString]); \
-}
+    STAssertEqualObjects([_URLObject absoluteString], _URLString, @"Expected absolute URL string @\"%@\" but got @\"%@\"", _URLString, [_URLObject absoluteString]);
 
-#define AssertExpectedBaseURL(_URLObject, _baseURL) { \
+#define AssertExpectedBaseURL(_URLObject, _baseURL) \
     STAssertNotNil(_URLObject, @"-[NSURL JL_URLWithProtocol:domain:endpoint:parameters:] returned a nil object."); \
-    STAssertEqualObjects([_URLObject baseURL], _baseURL, @"Expected base URL %@ but got %@", _baseURL, [_URLObject baseURL]); /* does this print nicely? */ \
-}
+    STAssertEqualObjects([_URLObject baseURL], _baseURL, @"Expected base URL %@ but got %@", _baseURL, [_URLObject baseURL]);
 
-#define AssertExpectedFragment(_URLObject, _fragment) { \
+#define AssertExpectedFragment(_URLObject, _fragment) \
     STAssertNotNil(_URLObject, @"-[NSURL JL_URLWithProtocol:domain:endpoint:parameters:] returned a nil object."); \
-    STAssertEqualObjects([_URLObject fragment], _fragment, @"Expected fragment @\"%@\" but got @\"%@\"", _fragment, [_URLObject fragment]); \
-}
+    STAssertEqualObjects([_URLObject fragment], _fragment, @"Expected fragment @\"%@\" but got @\"%@\"", _fragment, [_URLObject fragment]);
 
-#define AssertExpectedLastPathComponent(_URLObject, _lastPathComponent) { \
+#define AssertExpectedLastPathComponent(_URLObject, _lastPathComponent) \
     STAssertNotNil(_URLObject, @"-[NSURL JL_URLWithProtocol:domain:endpoint:parameters:] returned a nil object."); \
-    STAssertEqualObjects([_URLObject lastPathComponent], _lastPathComponent, @"Expected last path component @\"%@\" but got @\"%@\"", _lastPathComponent, [_URLObject lastPathComponent]); \
-}
+    STAssertEqualObjects([_URLObject lastPathComponent], _lastPathComponent, @"Expected last path component @\"%@\" but got @\"%@\"", _lastPathComponent, [_URLObject lastPathComponent]);
 
 
-#define AssertExpectedParameterString(_URLObject, _parameterString) { \
+#define AssertExpectedParameterString(_URLObject, _parameterString) \
     STAssertNotNil(_URLObject, @"-[NSURL JL_URLWithProtocol:domain:endpoint:parameters:] returned a nil object."); \
-    STAssertEqualObjects([_URLObject parameterString], _parameterString, @"Expected parameter string @\"%@\" but got @\"%@\"", _parameterString, [_URLObject parameterString]); \
-}
+    STAssertEqualObjects([_URLObject parameterString], _parameterString, @"Expected parameter string @\"%@\" but got @\"%@\"", _parameterString, [_URLObject parameterString]);
 
-
+#define AssertExpected(_URLObject, _host, _URLString, _baseURL, _fragment, _lastPathComponent, _parameterString) \
+    AssertExpectedHost(_URLObject, _host) \
+    AssertExpectedAbsoluteURLString(_URLObject, _URLString) \
+    AssertExpectedBaseURL(_URLObject, _baseURL) \
+    AssertExpectedFragment(_URLObject, _fragment) \
+    AssertExpectedLastPathComponent(_URLObject, _lastPathComponent) \
+    AssertExpectedParameterString(_URLObject, _parameterString)
 
 @implementation JLUNIT_Parameters
 
@@ -59,24 +59,51 @@
     [super tearDown];
 }
 
+- (void)testParameterValidation {
+    // nil domain
+    STAssertThrowsSpecificNamed([NSURL JL_URLWithProtocol:JL_URLProtocolHTTP domain:nil endpoint:nil parameters:nil],
+                                NSException, NSInternalInconsistencyException,
+                                @"+[NSURL JL_URLWithProtocol:domain:endpoint:parameters] did not bail when a nil domain parameter was specified.");
+    // protocol out of bounds
+    STAssertThrowsSpecificNamed([NSURL JL_URLWithProtocol:5 domain:@"example.com" endpoint:nil parameters:nil],
+                                NSException, NSInternalInconsistencyException,
+                                @"+[NSURL JL_URLWithProtocol:domain:endpoint:parameters] did not bail when an out of bounds protocol was specified.");
+}
+
 - (void)testSimpleURL {
-    NSURL *simpleURL = [NSURL JL_URLWithProtocol:JL_URLProtocolHTTP domain:@"google.com" endpoint:nil parameters:nil];
-    AssertExpectedHost(simpleURL, @"google.com");
-    AssertExpectedAbsoluteURLString(simpleURL, @"http://google.com");
-    AssertExpectedBaseURL(simpleURL, nil);
-    AssertExpectedFragment(simpleURL, nil);
-    AssertExpectedLastPathComponent(simpleURL, @"");
-    AssertExpectedParameterString(simpleURL, nil);
+    NSURL *simpleURL = [NSURL JL_URLWithProtocol:JL_URLProtocolHTTP domain:@"example.com" endpoint:nil parameters:nil];
+    AssertExpected(simpleURL, @"example.com", @"http://example.com", nil, nil, @"", nil);
 }
 
 - (void)testURLWithEndpoint {
-    NSURL *endpointURL = [NSURL JL_URLWithProtocol:JL_URLProtocolHTTP domain:@"google.com" endpoint:@"webhp" parameters:nil];
-    AssertExpectedHost(endpointURL, @"google.com");
-    AssertExpectedAbsoluteURLString(endpointURL, @"http://google.com/webhp");
-    AssertExpectedBaseURL(endpointURL, nil);
-    AssertExpectedFragment(endpointURL, nil);
-    AssertExpectedLastPathComponent(endpointURL, @"webhp");
-    AssertExpectedParameterString(endpointURL, nil);
+    NSURL *endpointURL = [NSURL JL_URLWithProtocol:JL_URLProtocolHTTP domain:@"example.com" endpoint:@"webhp" parameters:nil];
+    AssertExpected(endpointURL, @"example.com", @"http://example.com/webhp", nil, nil, @"webhp", nil);
+}
+
+- (void)testURLWithParameterNoEndpoint {
+    NSURL *paramURL = [NSURL JL_URLWithProtocol:JL_URLProtocolHTTP domain:@"example.com" endpoint:nil parameters:@{@"query": @"cats"}];
+    AssertExpected(paramURL, @"example.com", @"http://example.com?query=cats", nil, nil, @"", nil);
+}
+
+- (void)testURLWithParameterAndEndpoint {
+    NSURL *paramURL = [NSURL JL_URLWithProtocol:JL_URLProtocolHTTP domain:@"example.com" endpoint:@"images" parameters:@{@"query": @"cats"}];
+    AssertExpected(paramURL, @"example.com", @"http://example.com/images?query=cats", nil, nil, @"images", nil);
+
+}
+
+- (void)testURLWithParametersNoEndpoint {
+    NSURL *paramURL = [NSURL JL_URLWithProtocol:JL_URLProtocolHTTP domain:@"example.com" endpoint:nil parameters:@{@"query": @"cats", @"imageSize": @"500px"}];
+    AssertExpected(paramURL, @"example.com", @"http://example.com?query=cats&imageSize=500px", nil, nil, @"", nil);
+}
+
+- (void)testURLWithParametersAndEndpoint {
+    NSURL *paramURL = [NSURL JL_URLWithProtocol:JL_URLProtocolHTTP domain:@"example.com" endpoint:@"images" parameters:@{@"query": @"cats", @"imageSize": @"500px"}];
+    AssertExpected(paramURL, @"example.com", @"http://example.com/images?query=cats&imageSize=500px", nil, nil, @"images", nil);
+}
+
+- (void)testURLWithParametersAndLongEndpoint {
+    NSURL *paramURL = [NSURL JL_URLWithProtocol:JL_URLProtocolHTTP domain:@"example.com" endpoint:@"api/v1/images" parameters:@{@"query": @"cats", @"imageSize": @"500px"}];
+    AssertExpected(paramURL, @"example.com", @"http://example.com/api/v1/images?query=cats&imageSize=500px", nil, nil, @"images", nil);
 }
 
 @end
