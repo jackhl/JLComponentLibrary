3807deb6aceebc77f7ec65ad4517dcbffcd39b68
Jack Lawrence <jlawrence@mail.rit.edu>
2012-11-09 23:00:05 -0500

    Added test class for RequestDispatch.

---
+++ JLComponentLibrary_UnitTests/UnitTests/JLUNIT_RequestDispatch.h
@@ -0,0 +1,13 @@
+//
+//  JLUNIT_RequestDispatch.h
+//  JLComponentLibrary_UnitTests
+//
+//  Created by Jack Lawrence on 11/9/12.
+//  Copyright (c) 2012 Jack Lawrence. All rights reserved.
+//
+
+#import "JLUNIT_RequestDispatchCommon.h"
+
+@interface JLUNIT_RequestDispatch : JLUNIT_RequestDispatchCommon
+
+@end
---
+++ JLComponentLibrary_UnitTests/UnitTests/JLUNIT_RequestDispatch.m
@@ -0,0 +1,13 @@
+//
+//  JLUNIT_RequestDispatch.m
+//  JLComponentLibrary_UnitTests
+//
+//  Created by Jack Lawrence on 11/9/12.
+//  Copyright (c) 2012 Jack Lawrence. All rights reserved.
+//
+
+#import "JLUNIT_RequestDispatch.h"
+
+@implementation JLUNIT_RequestDispatch
+
+@end
