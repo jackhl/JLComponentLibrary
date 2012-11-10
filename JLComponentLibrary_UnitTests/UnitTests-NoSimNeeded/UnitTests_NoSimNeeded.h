10cb9721919dfe015d3bc13d0d472ebe82a98316
Jack Lawrence <jlawrence@mail.rit.edu>
2012-11-09 23:13:59 -0500

    Added cocoa target for no simulator testing.

---
+++ JLComponentLibrary_UnitTests/UnitTests-NoSimNeeded/UnitTests_NoSimNeeded.h
@@ -0,0 +1,13 @@
+//
+//  UnitTests_NoSimNeeded.h
+//  UnitTests-NoSimNeeded
+//
+//  Created by Jack Lawrence on 11/9/12.
+//  Copyright (c) 2012 Jack Lawrence. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface UnitTests_NoSimNeeded : SenTestCase
+
+@end
---
+++ JLComponentLibrary_UnitTests/UnitTests-NoSimNeeded/UnitTests_NoSimNeeded.m
@@ -0,0 +1,32 @@
+//
+//  UnitTests_NoSimNeeded.m
+//  UnitTests-NoSimNeeded
+//
+//  Created by Jack Lawrence on 11/9/12.
+//  Copyright (c) 2012 Jack Lawrence. All rights reserved.
+//
+
+#import "UnitTests_NoSimNeeded.h"
+
+@implementation UnitTests_NoSimNeeded
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in UnitTests-NoSimNeeded");
+}
+
+@end
