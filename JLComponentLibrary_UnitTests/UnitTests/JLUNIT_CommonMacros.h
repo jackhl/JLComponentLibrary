1cbe8d3b87e04369b70b75dbba2b6553fd9f39b4
Jack Lawrence <jlawrence@mail.rit.edu>
2012-11-12 12:17:21 -0500

    Wording.

--- JLComponentLibrary_UnitTests/UnitTests/JLUNIT_CommonMacros.h
+++ JLComponentLibrary_UnitTests/UnitTests/JLUNIT_CommonMacros.h
@@ -9,14 +9,15 @@
 #ifndef JLComponentLibrary_UnitTests_JLUNIT_CommonMacros_h
 #define JLComponentLibrary_UnitTests_JLUNIT_CommonMacros_h
 
-// Basically the problem is that STAssertThrowsSpecificNamed(...) prints a big assertion warning
-// when an assertion does not pass, even though in this case I'm specifically trying to check
-// to make sure that the expression does in fact throw an exception. It leads to some confusing
-// logs where OCUnit says that the test passed but immediately before that it says an assertion
-// failed. This leads to some confusion over whether the assertion failure was related to the
-// test or other code. Eventually I should try to make this cleaner.
+/* 
+ * Basically the problem is that STAssertThrowsSpecificNamed(...) prints a big assertion warning
+ * when an assertion does not pass, even though in this case I'm specifically trying to check
+ * to make sure that the expression does in fact throw an exception. It leads to some confusing
+ * logs where OCUnit says that the test passed but immediately before that it says an assertion
+ * failed. This leads to some confusion over whether the assertion failure was related to the
+ * test or other code. Eventually I should try to make this cleaner.
+ */
 #define JLUNIT_AssertThrowsSpecificNamed(_expr, _specificException, _aName, _description, ...) \
-    NSLog(@"EXPECT ASSERTION FAILURE"); \
+    NSLog(@"EXPECT RAISE ASSERTION"); \
     STAssertThrowsSpecificNamed(_expr, _specificException, _aName, _description, ##__VA_ARGS__)
-
 #endif
