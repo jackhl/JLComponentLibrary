05cfc074a9d11c8dfd604b5f7373fc92b7cd8e8c
Jack Lawrence <jlawrence@mail.rit.edu>
2012-11-12 13:18:53 -0500

    Updated comments.

--- Categories/NSString+WhiteSpace.h
+++ Categories/NSString+WhiteSpace.h
@@ -8,8 +8,18 @@
 
 #import <Foundation/Foundation.h>
 
+/** Extends NSString to provide support for detecting whitespace. */
 @interface NSString (WhiteSpace)
 
+/**
+ Determines whether or not the receiver does not only contain whitespace or is nil.
+ 
+ @note This method doesn't actually check for nil. It's worded awkwardly as such
+ (ie the absence of certain content results in TRUE instead of the presence) to
+ accomidate the fact that method calls on nil objects return nil.
+ 
+ @return Whether or not the receiver does not only contain whitespace or is nil.
+ */
 - (BOOL)isNotWhiteSpaceOrNil;
 
 @end
