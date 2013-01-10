b4a518b58398a98e0188b22cfd06dbaf528fc96a
Jack Lawrence <jlawrence@mail.rit.edu>
2013-01-10 12:14:30 -0500

    Added UI testing target.

---
+++ JLComponentLibrary_UnitTests/UITests/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UITests
+//
+//  Created by Jack Lawrence on 1/10/13.
+//  Copyright (c) 2013 Jack Lawrence. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JLUNITAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JLUNITAppDelegate class]));
+    }
+}
