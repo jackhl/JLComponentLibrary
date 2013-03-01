fb00c4c30436cccf8c3224f4e89630598a7fd01c
Jack Lawrence <jlawrence@mail.rit.edu>
2013-02-28 20:53:40 -0500

    Improved docs.

--- Categories/NSObject+JL_KeyPathIntrospection.h
+++ Categories/NSObject+JL_KeyPathIntrospection.h
@@ -7,6 +7,8 @@
 /** 
  Implements methods to simplify Objective-C runtime introspection on key paths.
  
+ ## Overview
+ 
  JL_KeyPathIntrospection is cool because instead of traveling a particular instance
  like `valueForKeyPath:` and related methods do, this category traverses the class
  definitions themselves.
@@ -16,7 +18,7 @@
  
  For example, JL_KeyPathIntrospection is useful for taking an object and then a 
  property list configuration file with key paths and various other pieces of 
- metadata and then rocessing the data or generating UI. I have personally used 
+ metadata and then processing the data or generating UI. I have personally used 
  this category to write a small framework that takes an arbitrary object and an 
  HTML template that contains marked-up keypaths and generates a PDF. It uses key 
  path introspection to smartly determine how to handle types like dates and 
@@ -25,12 +27,15 @@
  input views depending on the keypath data type which then automatically retrieved 
  and saved properties on the passed-in object.
  
- @note `id` is simply a typedef for a struct; It is not a full-fledged class.
+ ## Notes
+ 
+ `id` is simply a typedef for a struct; It is not a full-fledged class.
  Therefore when checking for type `id`, use the primitive introspection methods
  which will return `@` for properties declared as type `id`.
  
+ ## Warnings
  
- @warn Because the introspection is done on the class definition and not a particular
+ Because the introspection is done on the class definition and not a particular
  instance, introspection will dead-end when the property is of type id. It will 
  also report the declared object type, so for example if you externally declare
  a property of type `NSArray` but internally you return an `NSMutableArray`, 
@@ -39,9 +44,7 @@
  useful and efficient? I should check libextobjc and see if they do something
  similar.
  
- 
- @warn JL_KeyPathIntrospection does not look at private property (re)declarations.
- 
+ JL_KeyPathIntrospection does not look at private property (re)declarations.
  */
 @interface NSObject (JL_KeyPathIntrospection)
 
